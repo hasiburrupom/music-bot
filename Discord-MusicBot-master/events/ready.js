ee0fbce0139f9914f56c533b848bf7edab4b0c39
hasiburrupom <68180307+hasiburrupom@users.noreply.github.com>
2021-03-15 14:41:03 +0600

    Update ready.js

--- Discord-MusicBot-master/events/ready.js
+++ Discord-MusicBot-master/events/ready.js
@@ -1,6 +1,6 @@
 module.exports = async (client) => {
   console.log(`[API] Logged in as ${client.user.username}`);
   await client.user.setActivity("Paths Of Glory - By Rupom", {
-    type: "STREAMING",//can be LISTENING, WATCHING, PLAYING, STREAMING
+    type: "PLAYING",//can be LISTENING, WATCHING, PLAYING, STREAMING
   });
 };
