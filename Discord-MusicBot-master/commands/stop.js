3850166071d1cbf35907c1dcbb5b1c32cc89cd75
hasiburrupom <68180307+hasiburrupom@users.noreply.github.com>
2021-03-31 00:36:43 +0600

    Update stop.js

--- Discord-MusicBot-master/commands/stop.js
+++ Discord-MusicBot-master/commands/stop.js
@@ -13,7 +13,7 @@ module.exports = {
     const channel = message.member.voice.channel
     if (!channel)return sendError("I'm sorry but you need to be in a voice channel to play music!", message.channel);
     const serverQueue = message.client.queue.get(message.guild.id);
-    if (!serverQueue)return sendError("There is nothing playing that I could stop for you.", message.channel);
+    if (!serverQueue)return sendError("Stop Korar Kicho nai", message.channel);
    if(!serverQueue.connection)return
 if(!serverQueue.connection.dispatcher)return
      try{
@@ -21,7 +21,7 @@ if(!serverQueue.connection.dispatcher)return
       } catch (error) {
         message.guild.me.voice.channel.leave();
         message.client.queue.delete(message.guild.id);
-        return sendError(`:notes: The player has stopped and the queue has been cleared.: ${error}`, message.channel);
+        return sendError(`:notes: Tomar Jonno Stop Kore Dilam.: ${error}`, message.channel);
       }
     message.client.queue.delete(message.guild.id);
     serverQueue.songs = [];
